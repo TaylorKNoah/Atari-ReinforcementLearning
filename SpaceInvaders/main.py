a0c251d6d17f05d05cd33c4efd9733e02c3a2fed
Taylor <taylorknoah@gmail.com>
2024-02-21 09:06:05 -0800

    working test env w/ no model

--- SpaceInvaders/main.py
+++ SpaceInvaders/main.py
@@ -1,5 +1,28 @@
-from ale_py import ALEInterface
-from ale_py.roms import SpaceInvaders
+import gym
+import random
 
-ale = ALEInterface()
-ale.loadROM(SpaceInvaders)
+# Setup Environment
+env = gym.make('SpaceInvaders-v4', render_mode='human')
+env.action_space.seed(42)
+
+height, width, channels = env.observation_space.shape
+actions = env.action_space.n
+print(height, width, channels)
+print(actions)
+
+# Little Test w/ no Model
+def envTest(episodes: int):
+	for episode in range(episodes):
+		state = env.reset(seed=42)
+		done = False
+		score = 0
+
+		while not done:
+			env.render()
+			action = random.choice([0, 1, 2, 3, 4, 5])
+			observation, reward, done, truncated, info = env.step(action)
+			score += reward
+		print('Episode: {}, Score: {}'.format(episode, score))
+	env.close()
+
+envTest(1)
